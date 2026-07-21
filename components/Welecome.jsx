3cb239d57d49af89e7b526b12f1e64736f4acfcc
hesham <heshamabdulaziz88@gmail.com>
2026-07-20 20:36:48 -0700

    Add  style  using stylesheet

--- components/Welecome.jsx
+++ components/Welecome.jsx
@@ -1,22 +1,33 @@
 
-import { Text, View} from "react-native";
+import { Text, View,StyleSheet} from "react-native";
 function Welecome({country,name,style}){
 
  
 // Add inline style 
 
 return(
-<view style={style}>
-<Text style={{color:"white",fontSize:20,fontWeight:"bold"}} >welecome youre name is :{name}</Text>
-<Text style={{color:"white",fontSize:20,fontWeight:"bold"}}>And youre country is :{country}</Text>
+<View style={style}>
+<Text style={styles.txt1} >welecome youre name is :{name}</Text>
+<Text style={styles.txt2}>And youre country is :{country}</Text>
 
-</view>
+</View>
 
 
 )
 
+ }
+  
  
+ 
+ { 
 
+// Add  style  using stylesheet
 
 }
+
+const styles=StyleSheet.create({
+txt1:{color:"white",fontSize:20,fontWeight:"bold"},
+txt2:{color:"yellow",fontSize:20,fontWeight:"bold"}
+
+})
 export default Welecome;
